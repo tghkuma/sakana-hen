e909311aebeeccb8de28bf4ecc26615ca1307a2a
tghkuma <info@team-grasshopper.info>
2024-02-14 22:42:56 +0900

    feat: サムネイルをhtmlに埋め込む
    * viteからHTMLを置換処理
    
    closed #26

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,21 @@
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const htmlPlugin = () => ({
+    name: 'html-transform',
+    transformIndexHtml: (html) => html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
+  })
+
   return {
     root: 'src',
     base: './',
     build: {
       emptyOutDir: true,
       outDir: '../dist',
-/*
+      /*
       assetsInlineLimit: 0, // asset が自動的に埋め込まれてしまうのを防ぐ
       // viteのbuild時のファイル名ハッシュを辞める
       rollupOptions: {
@@ -21,9 +28,10 @@ export default defineConfig(() => {
 */
     },
     plugins: [
+      htmlPlugin(),
       ViteImageOptimizer({
         /* pass your config */
       }),
     ],
-  };
-});
+  }
+})
